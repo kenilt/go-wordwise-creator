1c3e5dca39184af40102c8f7ea68c47c89d1da04
Thang Nguyen <nhthang.khtn@gmail.com>
2023-04-23 00:58:11 +0700

    Process the book with wordwise

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ const (
 	TempBookName           = "book_dump"
 )
 
+var specialChars = []string{",", "<", ">", ";", "&", "*", "~", "/", "\"", "[", "]", "#", "?", "`", "–", ".", "'", "!", "“", "”", ":", "."}
 var hintLevel int = 5
 var inputPath string
 var ebookConvertCmd string
@@ -42,16 +43,14 @@ func main() {
 	}
 	log.Printf("[+] Hint level: %d\n", hintLevel)
 
-	log.Println("[+] Load stopwords...")
-	// stopWords := loadStopWords()
-	loadStopWords()
+	log.Println("[+] Load stopwords")
+	stopWords := loadStopWords()
 
-	log.Println("[+] Load wordwise dict...")
-	// dict := loadWordwiseDict()
-	loadWordwiseDict()
+	log.Println("[+] Load wordwise dict")
+	dict := loadWordwiseDict()
 
 	// clean old temp
-	log.Println("[+] Cleaning old temp files...")
+	log.Println("[+] Cleaning old temp files")
 	cleanTempData()
 
 	// get ebook convert cmd
@@ -61,13 +60,113 @@ func main() {
 	createTempFolder()
 	convertBookToHtml(inputPath)
 
+	// process book
+	processHtmlBookData(stopWords, dict)
+
 	// create wordwise book
 	createBookWithWordwised(inputPath)
 
-	log.Println("[+] Cleaning temp files...")
+	// log.Println("[+] Cleaning temp files")
 	cleanTempData()
 
 	log.Println("--> Finished!")
+}
+
+type ProcessState int
+
+const (
+	OpenTag ProcessState = iota
+	Collecting
+)
+
+func processHtmlBookData(stopWords *map[string]bool, dict *map[string]DictRow) {
+	htmlBookPath := fmt.Sprintf("%s/%s/index1.html", TempDir, TempBookName)
+
+	bbytes, err := os.ReadFile(htmlBookPath)
+	if err != nil {
+		log.Fatalln("Error when open ", htmlBookPath, "->", err)
+	}
+
+	chars := []rune(string(bbytes))
+	var bookBuilder strings.Builder
+
+	state := Collecting
+	var collectBuilder strings.Builder
+	var tagBuilder strings.Builder
+	isSawBody := false
+	for i := 0; i < len(chars); i++ {
+		char := chars[i]
+		if char == '<' { // see the open tag mean collecting finish, process what was collected
+			state = OpenTag
+			collected := collectBuilder.String()
+			trimmed := strings.TrimSpace(collected)
+			if isSawBody && len(trimmed) > 0 {
+				processed := processBlock(trimmed, stopWords, dict)
+				bookBuilder.WriteString(processed)
+			} else {
+				bookBuilder.WriteString(collected)
+			}
+			collectBuilder.Reset()
+			bookBuilder.WriteRune(char)
+		} else if char == '>' { // see the close tag mean the tag content finish
+			state = Collecting
+			collectedTag := tagBuilder.String()
+			if !isSawBody && strings.HasPrefix(collectedTag, "body") {
+				isSawBody = true
+			}
+			tagBuilder.Reset()
+			bookBuilder.WriteString(collectedTag)
+			bookBuilder.WriteRune(char)
+		} else {
+			if state == Collecting {
+				collectBuilder.WriteRune(char)
+			} else if state == OpenTag {
+				tagBuilder.WriteRune(char)
+			}
+		}
+	}
+
+	fo, err := os.Create(htmlBookPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fo.Close()
+
+	_, err2 := fo.WriteString(bookBuilder.String())
+
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+}
+
+func processBlock(content string, stopWords *map[string]bool, dict *map[string]DictRow) string {
+	words := strings.Fields(content)
+	for i := 0; i < len(words); i++ {
+		word := words[i]
+		if _, ok := (*stopWords)[word]; ok {
+			continue
+		}
+
+		ws, ok := (*dict)[word]
+		if !ok {
+			continue
+		}
+
+		if ws.HintLv > hintLevel {
+			continue
+		}
+
+		words[i] = fmt.Sprintf("<ruby>%v<rt>%v</rt></ruby>", word, ws.ShortDef)
+	}
+	return strings.Join(words, " ")
+}
+
+// Remove special characters from word
+func cleanWord(word string) string {
+	replacer := strings.NewReplacer(specialChars...)
+	cleanWord := strings.ToLower(replacer.Replace(word))
+
+	return cleanWord
 }
 
 func cleanTempData() {
@@ -82,7 +181,7 @@ func createTempFolder() {
 }
 
 func convertBookToHtml(inputPath string) {
-	log.Println("[+] Convert Book to HTML")
+	log.Println("[+] Convert Book to HTML...")
 	tempBookPath := TempDir + "/" + TempBookName
 	runCommand(ebookConvertCmd, inputPath, (tempBookPath + ".htmlz"))
 	runCommand(ebookConvertCmd, (tempBookPath + ".htmlz"), tempBookPath)
@@ -98,13 +197,13 @@ func createBookWithWordwised(inputPath string) {
 	fileName := strings.TrimSuffix(filepath.Base(inputPath), extension)
 	extension = strings.Trim(extension, ".")
 
-	log.Println("[+] Create New Book with Wordwised")
+	log.Println("[+] Create New Book with Wordwised...")
 	htmlPath := fmt.Sprintf("%s/%s/index1.html", TempDir, TempBookName)
 	outputPath := fmt.Sprintf("%s/%s-wordwise.%s", bookPath, fileName, extension)
 	metaDataPath := fmt.Sprintf("%s/%s/content.opf", TempDir, TempBookName)
 	runCommand(ebookConvertCmd, htmlPath, outputPath, "-m", metaDataPath)
 
-	log.Println("[+] The EPUB book with wordwise generated at", outputPath)
+	log.Println("[+] The EPUB book with wordwise was generated at", outputPath)
 }
 
 func runCommand(name string, arg ...string) {
@@ -243,6 +342,6 @@ func loadWordwiseDict() *map[string]DictRow {
 		count++
 	}
 
-	log.Println("--> csv words:", count)
+	log.Println("--> Csv words:", count)
 	return &dict
 }
