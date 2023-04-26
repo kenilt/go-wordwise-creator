7190f501cbf7a92cf3dde768ffd94d07a59824b8
Thang Nguyen <nhthang.khtn@gmail.com>
2023-04-27 00:00:06 +0700

    Correct the autobuild.sh

--- autobuild.sh
+++ autobuild.sh
@@ -7,7 +7,7 @@ go mod download
 GOOS=darwin GOARCH=amd64 go build
 
 # create zip file for MacOS
-[ -f go-wordwise-creator-macos ] && rm -r go-wordwise-creator-macos
+rm -rf go-wordwise-creator-macos
 mkdir go-wordwise-creator-macos
 cp -R resources go-wordwise-creator-macos
 mv go-wordwise-creator go-wordwise-creator-macos
@@ -18,7 +18,7 @@ rm -r go-wordwise-creator-macos
 GOOS=windows GOARCH=amd64 go build
 
 # create zip file for Windows
-[ -f go-wordwise-creator-windows ] && rm -r go-wordwise-creator-windows
+rm -rf go-wordwise-creator-windows
 mkdir go-wordwise-creator-windows
 cp -R resources go-wordwise-creator-windows
 mv go-wordwise-creator.exe go-wordwise-creator-windows
@@ -26,7 +26,7 @@ zip -r go-wordwise-creator-windows.zip go-wordwise-creator-windows/
 rm -r go-wordwise-creator-windows
 
 # Move build to /build-outputs
-[ -f build-outputs ] && rm -r build-outputs
+rm -rf build-outputs
 mkdir build-outputs
 mv go-wordwise-creator-macos.zip build-outputs
 mv go-wordwise-creator-windows.zip build-outputs
