04270537f40ff1ab06ff36675480c903b258979c
Thang Nguyen <nhthang.khtn@gmail.com>
2023-05-03 10:24:53 +0700

    Update script to add a build for mac arm

--- autobuild.sh
+++ autobuild.sh
@@ -3,16 +3,27 @@
 # update dependencies
 go mod download
 
-# build for MacOS
+# build for MacOS intel
 GOOS=darwin GOARCH=amd64 go build
 
-# create zip file for MacOS
-rm -rf go-wordwise-creator-macos
-mkdir go-wordwise-creator-macos
-cp -R resources go-wordwise-creator-macos
-mv go-wordwise-creator go-wordwise-creator-macos
-zip -r go-wordwise-creator-macos.zip go-wordwise-creator-macos/
-rm -r go-wordwise-creator-macos
+# create zip file for MacOS intel
+rm -rf go-wordwise-creator-macos-amd64
+mkdir go-wordwise-creator-macos-amd64
+cp -R resources go-wordwise-creator-macos-amd64
+mv go-wordwise-creator go-wordwise-creator-macos-amd64
+zip -r go-wordwise-creator-macos-amd64.zip go-wordwise-creator-macos-amd64/
+rm -r go-wordwise-creator-macos-amd64
+
+# build for MacOS arm
+GOOS=darwin GOARCH=arm64 go build
+
+# create zip file for MacOS arm
+rm -rf go-wordwise-creator-macos-arm64
+mkdir go-wordwise-creator-macos-arm64
+cp -R resources go-wordwise-creator-macos-arm64
+mv go-wordwise-creator go-wordwise-creator-macos-arm64
+zip -r go-wordwise-creator-macos-arm64.zip go-wordwise-creator-macos-arm64/
+rm -r go-wordwise-creator-macos-arm64
 
 # build for Windows
 GOOS=windows GOARCH=amd64 go build
@@ -28,5 +39,6 @@ rm -r go-wordwise-creator-windows
 # Move build to /build-outputs
 rm -rf build-outputs
 mkdir build-outputs
-mv go-wordwise-creator-macos.zip build-outputs
+mv go-wordwise-creator-macos-amd64.zip build-outputs
+mv go-wordwise-creator-macos-arm64.zip build-outputs
 mv go-wordwise-creator-windows.zip build-outputs
